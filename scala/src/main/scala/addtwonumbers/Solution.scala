f426d4a858d9ed2308a332b74261dea258918cd5
George Gondim <george.gondim@nubank.com.br>
2021-05-27 09:24:30 -0300

    AddTwoNumbers

---
+++ scala/src/main/scala/addtwonumbers/Solution.scala
@@ -0,0 +1,42 @@
+/**
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order, and each of their nodes contains a single digit.
+ * Add the two numbers and return the sum as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *
+ * https://leetcode.com/problems/add-two-numbers/
+ */
+package addtwonumbers
+
+object Solution {
+  class ListNode(_x: Int = 0, _next: ListNode = null) {
+    var next: ListNode = _next
+    var x: Int = _x
+  }
+
+  def addTwoNumbers(l1: ListNode, l2: ListNode): ListNode = {
+
+    def loop(l1: ListNode, l2: ListNode, carryOne: Int): ListNode = {
+
+      if (l1 == null && l2 == null && carryOne == 0)
+        return null
+
+      def getNext(l1: ListNode): (Int, ListNode) = {
+        if (l1 == null) (0, null)
+        else (l1.x, l1.next)
+      }
+
+      val (x1, next1) = getNext(l1)
+      val (x2, next2) = getNext(l2)
+
+      val sum = (x1 + x2 + carryOne)
+
+      val x =  sum % 10
+      val newCarry = sum / 10
+
+      new ListNode(x, loop(next1, next2, newCarry))
+    }
+
+    loop(l1, l2, 0)
+  }
+}
--- scala/src/main/scala/twosum/Solution.scala
+++ scala/src/main/scala/twosum/Solution.scala
@@ -1,3 +1,9 @@
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they
+ * add up to target. You may assume that each input would have exactly one solution, and you may not use
+ * the same element twice. You can return the answer in any order.
+ * https://leetcode.com/problems/two-sum/
+ */
 package twosum
 
 object Solution extends App {
@@ -18,7 +24,6 @@ object Solution extends App {
 
     Array(0, 0)
   }
-
 
   println(twoSum(Array(2, 7, 11, 15), 9).toSeq.toString)
 }
